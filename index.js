1f303ab31c7596d6438efbb3e3746a821cc98808
splaza28 <global-splaza28@gmail.com>
2021-11-03 23:29:16 -0500

    added questions array

--- index.js
+++ index.js
@@ -5,7 +5,55 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "How would you describe your project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the instructions to install this project?",
+        name: "installation" 
+
+    },
+    {
+        type: "input",
+        message: "How can others contribute to this project?",
+        name: "contributions" 
+    },
+    {
+        type: "input",
+        message: "How can testing be conducted for this project?",
+        name: "testing" 
+    },
+    {
+        type: "input",
+        message: "What are the instructions for utilizing this project?",
+        name: "usage" 
+    },
+    {
+        type: "list",
+        message: "Which license did you use for this project?",
+        name: "licenses",
+        choices: ['MIT License', 'Apache License', 'GPL License'] 
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github" 
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email" 
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
